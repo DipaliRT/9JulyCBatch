6c6e4dbddff098ebcd6ab4001ef4aaf3fd60b653
Dip <dipali@eaxample.com>
2022-12-07 14:52:06 +0530

    sapce are added to base class

--- src/test/java/neoStoxBase/Base.java
+++ src/test/java/neoStoxBase/Base.java
@@ -19,12 +19,14 @@ public class Base
 		System.setProperty("webdriver.chrome.driver", "D:\\Velocity Group\\Automation 9jluy\\Selenium\\chromedriver.exe");	
 
 		driver = new ChromeDriver();
+		
 		driver.get(Utility.readDataFromPropertyFile("url"));
+		
 	    driver.manage().window().maximize();   
 	    
 		Utility.waitt(driver, 1000);
 		
-		Reporter.log("Lunching Browser...", false);
+		Reporter.log("Lunching Browser...", true);
 		
 	}
 	
